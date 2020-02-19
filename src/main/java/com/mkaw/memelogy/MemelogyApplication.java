c352f4bf35f668bfef32c82fb1838de20d678043
Aleksander Wardzinski <awardzinski@ptc.com>
2020-02-19 09:52:34 +0100

    Generate project with initial dependencies

---
+++ src/main/java/com/mkaw/memelogy/MemelogyApplication.java
@@ -0,0 +1,13 @@
+package com.mkaw.memelogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemelogyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemelogyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkaw/memelogy/MemelogyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkaw.memelogy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemelogyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
