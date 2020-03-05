f915c1b9c48ac068efe129c931c53174a1a98e27
michał_krzywański <michak964@gmail.com>
2020-03-05 18:03:12 +0100

    Adding initial checkstyle rules (#4)

--- memelogy-backend/src/main/java/com/mkaw/memelogy/MemelogyApplication.java
+++ memelogy-backend/src/main/java/com/mkaw/memelogy/MemelogyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MemelogyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemelogyApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(MemelogyApplication.class, args);
+    }
 
 }
